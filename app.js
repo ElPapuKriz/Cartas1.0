a734369b130bc44bc1dca44dced050b2f2bb4670
IronChris <pazochris@gmail.com>
2025-04-20 20:29:40 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let Producto_seleccionado =document.querySelector(".lista");
+
+Producto_seleccionado.addEventListener("click",()=>{
+
+alert("Haz presionado en este elemento.");
+
+});
+
+function Sumar(){
+
+  let parrafo = document.getElementById("numero_contador");
+  let numero = parseInt(parrafo.textContent);
+  numero++;
+  document.getElementById("numero_contador").innerHTML=numero;
+
+  numero < 100 ? "":alert("Felicidades haz clickeado 100 veces !");
+  
+}
+
+function Reset(){
+
+  document.getElementById("numero_contador").innerHTML="0";
+
+}
